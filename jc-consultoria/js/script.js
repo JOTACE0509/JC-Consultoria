49fae35d4cd25d039a6360e297da8f6c8865f836
Júlio César <87682376+JOTACE0509@users.noreply.github.com>
2021-11-13 12:00:27 -0300

    Add files via upload
    Commit para confirmar o upload dos arquivos

---
+++ jc-consultoria/js/script.js
@@ -0,0 +1,18 @@
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide: 'clientes',
+    time: 5000, 
+    pauseOnHover: true,
+    nav: true
+  });
+  new SimpleSlide({
+    slide: 'servicos',
+    time: 5000, 
+    pauseOnHover: true,
+    nav: true
+  });
+}
+
+if (window.SimpleAnime) {
+	new SimpleAnime();
+}
